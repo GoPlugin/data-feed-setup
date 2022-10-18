3adf6fefbbdcb2531c795e250c11dd1cc8136019
satz07 <39335466+satz07@users.noreply.github.com>
2022-10-18 13:49:53 +0400

    Update index.js
    Updated version

--- adapter/index.js
+++ adapter/index.js
@@ -1,35 +1,39 @@
 const { Requester, Validator } = require('@goplugin/external-adapter')
+require("dotenv").config();
 
 const customError = (data) => {
-    if (data.Response === 'Error') return true
-    return false
+  if (data.Response === 'Error') return true
+  return false
 }
 
 const createRequest = (input, callback) => {
-    const jobRunID = input.id;
-    
-    console.log("log:::values:::",input);
-    
-    const params = {
-        fsym: input.data._fsysm,
-        tsyms: input.data._tsysm,
+
+const url = `https://goplugin.apidiscovery.teejlab.com/edsn/api/gateway?endpoint_id=aHBrfib`
+
+var dataString = {"fsyms": `${input.data._fsyms}`, "tsyms": `${input.data._tsyms}`};
+const config = {
+    url,
+    method : "POST",
+    data : dataString,
+  }
+
+  if (process.env.API_KEY) {
+    config.headers = {
+      "api-key": process.env.API_KEY
     }
-    const config = {
-        url: "https://min-api.cryptocompare.com/data/price",
-        params
-    }
-    Requester.request(config, customError)
-        .then(response => {
-            const res = {
-              data: {
-                "result": response.data.USD.toString()
-              }
-            }
-            callback(response.status, {jobRunID, ...res});
-        })
-        .catch(error => {
-            callback(500, Requester.errored(jobRunID, error))
-        })
+  }
+  Requester.request(config, customError)
+    .then(response => {
+      const res = {
+        data: {
+         "result": response.data[`${input.data._fsyms}`][`${input.data._tsyms}`].toString()
+}
+      }
+      callback(response.status, Requester.success(input.id, res));
+    })
+    .catch(error => {
+      callback(500, Requester.errored(input.id, error))
+    })
 }
 
 module.exports.createRequest = createRequest
